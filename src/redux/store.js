55a59a4e7e942a39c51c695953ced9fd545552ed
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-28 09:02:22 +0300

    new changes
    new changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,9 @@
-//import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+
+import { contactsReducer } from './slice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+});
